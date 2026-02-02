a3ec4cc04f022305217b27e4559930ff7b64a189
Jagadeeswari1785 <164888609+Jagadeeswari1785@users.noreply.github.com>
2026-02-02 18:19:19 +0530

    Implement input handling and size comparison features
    Added multiple functionalities including input handling, size of data types, and comparison of two numbers.

--- DAY01/Day1mysolution.java
+++ DAY01/Day1mysolution.java
@@ -1,6 +1,28 @@
+//
 import java.util.Scanner;
 public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
     int a=sc.nextInt();
     System.out.print("You entered :" +a);
+  }
+//print the size
+import java.util.Scanner;
+public static void main(String args[]){
+    System.out.println("Size of int "+Integer.BYTES);
+    System.out.println("Size of float "+Float.BYTES);
+    System.out.println("Size of Double "+Double.BYTES);
+    System.out.print("Size of char "+Character.BYTES);
+  }
+//Find the Larger Number Among Two Numbers
+import java.util.Scanner;
+public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    if(a>b){
+      System.out.print(a+" is greater");
+    }
+    else{
+      System.out.print(b+" is greater");
+    }
   }
