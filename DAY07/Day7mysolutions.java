ada070d25dae5e30bc25a18af179dd2c6b90d6b5
Jagadeeswari1785 <164888609+Jagadeeswari1785@users.noreply.github.com>
2026-02-03 22:53:55 +0530

    Create Day7mysolutions.java

---
+++ DAY07/Day7mysolutions.java
@@ -0,0 +1,19 @@
+//Pattern 1: Inverted Right-angled triangle
+import java.util.Scanner;
+class Hello{
+public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  int k=n;
+  for(int i=0;i<k;i++){
+    for(int j=0;j<n;j++)
+      {
+        System.out.print("*");
+      }
+      n--;
+      System.out.println();
+    }
+    
+  }
+  }
+//
