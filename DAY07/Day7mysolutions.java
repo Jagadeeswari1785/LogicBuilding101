3a38789dd58f6daff1304b60527e2416e407ceac
Jagadeeswari1785 <164888609+Jagadeeswari1785@users.noreply.github.com>
2026-02-07 22:29:09 +0530

    Add pyramid pattern printing in Day7mysolutions.java

--- DAY07/Day7mysolutions.java
+++ DAY07/Day7mysolutions.java
@@ -16,4 +16,23 @@ public static void main(String args[]){
     
   }
   }
-//
+//Pyramid
+import java.util.Scanner;
+class Hello{
+public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  for(int i=0;i<=n;i++){
+    int space=n-i;
+    int star=2*i-1;
+    for(int j=0;j<space;j++){
+      System.out.print(" ");
+    }
+    for(int k=0;k<star;k++){
+      System.out.print("*");
+    }
+    System.out.println();
+    
+  }   
+    }
+}
