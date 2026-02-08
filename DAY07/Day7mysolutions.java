5babbd99af9854d478130540eb78e2ee40f7450b
Jagadeeswari1785 <164888609+Jagadeeswari1785@users.noreply.github.com>
2026-02-08 10:16:20 +0530

    Update Day7mysolutions.java

--- DAY07/Day7mysolutions.java
+++ DAY07/Day7mysolutions.java
@@ -36,3 +36,26 @@ public static void main(String args[]){
   }   
     }
 }
+//Inverted pyramid
+import java.util.Scanner;
+class Hello{
+public static void main(String args[]){
+  Scanner sc=new Scanner(System.in);
+  int n=sc.nextInt();
+  int star=n+(n-1);
+  for(int i=0;i<=n;i++){
+    int space=i;
+    
+    for(int j=0;j<space;j++){
+      System.out.print(" ");
+    }
+    for(int k=0;k<star;k++){
+      System.out.print("*");
+    }
+    System.out.println();
+    space++;
+    star=star-2;
+    
+  } 
+    }
+}
